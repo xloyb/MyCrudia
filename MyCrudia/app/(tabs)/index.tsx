d08b511e4ecd3b1cfaff1e75a78bb8283a93d807
mydevify <boussaadilouay9@gmail.com>
2024-12-25 23:04:42 +0100

    fixed a mistake

--- MyCrudia/app/(tabs)/index.tsx
+++ MyCrudia/app/(tabs)/index.tsx
@@ -1,51 +1,17 @@
-import { Button, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import { AuthProvider, useAuth } from '../context/AuthContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/Home';
-import Login from '../screens/Login';
-
-const Stack = createNativeStackNavigator();
 
 export default function TabOneScreen() {
   return (
-    <AuthProvider>
-      <View style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
-    </AuthProvider>
   );
 }
-
-
-export const Layout2 = () => {
-  const { authState, onLogout } = useAuth();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {authState?.authenticated ? (
-          <Stack.Screen name="Home" component={Home} options={
-            {
-              headerRight: () => (
-                <Button title="Logout" onPress={onLogout}></Button>
-              )
-            }
-          }></Stack.Screen>
-        ) : (
-          <Stack.Screen name="Login" component={Login} ></Stack.Screen>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-  
-}
-
 
 const styles = StyleSheet.create({
   container: {
