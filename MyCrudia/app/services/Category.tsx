0adeede2db807307e90e572caca8e35dfe64ac5b
mydevify <boussaadilouay9@gmail.com>
2024-12-27 20:06:19 +0100

    created getAuthToken to send the JWT token with each request

--- MyCrudia/app/services/Category.tsx
+++ MyCrudia/app/services/Category.tsx
@@ -1,11 +1,21 @@
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
 import { API_URL } from '../context/AuthContext';
 
 const CATEGORY_URL = `${API_URL.replace('/auth', '/categories')}`;
+const TOKEN_KEY = "my-jwt";
 
+async function getAuthToken() {
+  return await SecureStore.getItemAsync(TOKEN_KEY);
+}
+
+// Create Category
 export const createCategory = async (name: string) => {
   try {
-    const response = await axios.post(CATEGORY_URL, { name });
+    const token = await getAuthToken();
+    const response = await axios.post(CATEGORY_URL, { name }, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (error) {
     console.error('Error creating category:', error);
@@ -13,9 +23,13 @@ export const createCategory = async (name: string) => {
   }
 };
 
+// Get All Categories
 export const getCategories = async () => {
   try {
-    const response = await axios.get(CATEGORY_URL);
+    const token = await getAuthToken();
+    const response = await axios.get(CATEGORY_URL, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (error) {
     console.error('Error fetching categories:', error);
@@ -23,9 +37,13 @@ export const getCategories = async () => {
   }
 };
 
+// Update Category
 export const updateCategory = async (id: number, name: string) => {
   try {
-    const response = await axios.put(`${CATEGORY_URL}/${id}`, { name });
+    const token = await getAuthToken();
+    const response = await axios.put(`${CATEGORY_URL}/${id}`, { name }, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (error) {
     console.error('Error updating category:', error);
@@ -33,9 +51,13 @@ export const updateCategory = async (id: number, name: string) => {
   }
 };
 
+// Delete Category
 export const deleteCategory = async (id: number) => {
   try {
-    await axios.delete(`${CATEGORY_URL}/${id}`);
+    const token = await getAuthToken();
+    await axios.delete(`${CATEGORY_URL}/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
   } catch (error) {
     console.error('Error deleting category:', error);
     throw error;
