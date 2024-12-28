1501ae87b19b3c708395e2382fd7f4df20cba993
mydevify <boussaadilouay9@gmail.com>
2024-12-28 01:09:48 +0100

    created TabBarIconMaterialIcons function for icons

--- MyCrudia/app/(tabs)/_layout.tsx
+++ MyCrudia/app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useAuth } from '../context/AuthContext';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -14,6 +15,13 @@ function TabBarIcon(props: {
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function TabBarIconMaterialIcons(props: {
+  name: React.ComponentProps<typeof MaterialIcons>['name'];
+  color: string;
+}) {
+  return <MaterialIcons size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
@@ -34,7 +42,7 @@ export default function TabLayout() {
         options={{
           title: 'MyCrudia',
           headerRight: () => <Button title="Logout" onPress={onLogout} />,
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
          
         }}
       />
@@ -42,7 +50,7 @@ export default function TabLayout() {
         name="two"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIconMaterialIcons name="category" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
@@ -59,7 +67,13 @@ export default function TabLayout() {
           ),
         }}
       />
-
+<Tabs.Screen
+        name="manageproducts"
+        options={{
+          title: 'sdfsdfsd',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
 <Tabs.Screen
         name="managecategories"
         options={{
@@ -67,7 +81,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+
+
+
+      
     </Tabs>
+
+
+
   );
 }
 
